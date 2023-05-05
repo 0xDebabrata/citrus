037dacdc86fd41663df29f8297195dc27bb43f8d
Debabrata Mondal <debabrata.js@protonmail.com>
2023-05-06 00:25:59 +0530

    v 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="citrusdb",
-    version="0.1.3",
+    version="0.2.0",
     author="Debabrata Mondal",
     author_email="debabrata.js@protonmail.com",
     description="(distributed) vector database",
