991bf96b82a1f0e4eb8c1cfb81f59e53f1f818c4
Debabrata Mondal <debabrata.js@protonmail.com>
2023-06-11 23:52:57 +0530

    push to v0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="citrusdb",
-    version="0.3.1",
+    version="0.3.3",
     author="Debabrata Mondal",
     author_email="debabrata.js@protonmail.com",
     description="(distributed) vector database",
