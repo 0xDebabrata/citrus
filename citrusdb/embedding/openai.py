7977ec62636479f0a148557616fbd030c603947a
Debabrata Mondal <debabrata.js@protonmail.com>
2024-01-12 20:15:20 +0530

    openai migration

--- citrusdb/embedding/openai.py
+++ citrusdb/embedding/openai.py
@@ -8,7 +8,5 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def get_embeddings(data: List[str], model="text-embedding-ada-002"):
     strings = [text.replace("\n", " ") for text in data]
-
-    response = openai.Embedding.create(model=model, input=strings)
-
-    return np.array([result["embedding"] for result in response["data"]], dtype=np.float32)
+    response = openai.embeddings.create(model=model, input=strings)
+    return np.array([result.embedding for result in response.data], dtype=np.float32)
